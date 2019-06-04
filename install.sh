2e9bbf7ca96d7ffd6c180ca6046f7ebe156c8824
James <swedeenjames@gmail.com>
2019-06-03 20:04:19 -0600

    started making templates

--- install.sh
+++ install.sh
@@ -1,11 +1,25 @@
 #! /bin/bash
 
-echo "Extracting tar files."
+echo "Extracting tar files"
+echo 
 
 mkdir camunda-engine
-tar -xzf ./camunda-bpm-tomcat-7.11.0.tar.gz -C camunda-engine > /dev/null
-tar -xzf ./camunda-modeler-3.1.0-linux-x64.tar.gz > /dev/null
-mv ./camunda-modeler-3.1.0-linux-x64 ./camunda-modeler > /dev/null
+tar -xzf ./camunda-bpm-tomcat-7.11.0.tar.gz -C camunda-engine 
+tar -xzf ./camunda-modeler-3.1.0-linux-x64.tar.gz 
+mv ./camunda-modeler-3.1.0-linux-x64 ./camunda-modeler 
 
+echo 
+echo "Downloading Plugins"
+echo 
+
+git clone https://github.com/bpmn-io/bpmn-js-token-simulation-plugin.git ./camunda-modeler/resources/plugins/token-sim/ 
+
+echo
+echo "Installing element templates"
+echo
+
+cp -r ./element-templates ./camunda-modeler/resources/
+
+echo 
 echo "Done"
 
